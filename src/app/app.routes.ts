b11d154e172cb0f7f0ebb63b1b53ecb799537e8f
Adriel747 <adriel.fernandez@ucb.edu.bo>
2024-09-16 07:32:26 -0400

    se crea y visualia la creacion de la materia

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { AgregarNuevoContenidoComponent } from './agregar-nuevo-contenido/agregar-nuevo-contenido.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+    path: 'agregar-nuevo-contenido',
+    component: AgregarNuevoContenidoComponent,  // Aquí registras tu componente en las rutas
+    title: 'Agregar Nuevo Contenido'
+    }
+];
