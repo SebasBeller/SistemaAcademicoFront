21d282fecad4f69776620a7cd6df715ac0ddd26d
Marlon Tomas Marzo Fernandez <marlonmarzo2@gmail.com>
2024-09-18 14:31:35 -0400

    Subir_Material

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,9 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync()]
+  providers: [provideRouter(routes), provideAnimationsAsync(), provideFirebaseApp(() => initializeApp({"projectId":"file-update-3ec62","appId":"1:865106620028:web:a3e9cc311723a3f556e3f9","storageBucket":"file-update-3ec62.appspot.com","apiKey":"AIzaSyDwAxeEhEOgc6_gnf9NMKbEo9d1v1DGaaE","authDomain":"file-update-3ec62.firebaseapp.com","messagingSenderId":"865106620028"})), provideStorage(() => getStorage())]
 };
