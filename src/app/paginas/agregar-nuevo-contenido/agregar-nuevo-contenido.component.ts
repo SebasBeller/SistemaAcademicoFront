61a5fd441cbf786df7f4ea84f42cd8029d631119
Adriel747 <adriel.fernandez@ucb.edu.bo>
2024-09-19 16:47:16 -0400

    actualizacion de mostrar contenido

--- src/app/paginas/agregar-nuevo-contenido/agregar-nuevo-contenido.component.ts
+++ src/app/paginas/agregar-nuevo-contenido/agregar-nuevo-contenido.component.ts
@@ -1,22 +1,39 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
 @Component({
   selector: 'app-agregar-nuevo-contenido',
   standalone: true,
   templateUrl: './agregar-nuevo-contenido.component.html',
-  styleUrl: './agregar-nuevo-contenido.component.sass',
-  imports: [CommonModule]
+  styleUrls: ['./agregar-nuevo-contenido.component.sass'],
+  imports: [CommonModule, FormsModule]
 })
 export class AgregarNuevoContenidoComponent {
+  showForm = false;
+  newModuleName = '';
+  newModuleImageUrl = '';
   cardCounter = 1;
-  cards: { id: number; content: string }[] = [];
+  cards: { id: number; content: string; imageUrl: string }[] = [];
 
-  addCard() {
+  addNewModule() {
     this.cards.push({
       id: this.cardCounter,
-      content: `Nuevo Contenido ${this.cardCounter}`
+      content: this.newModuleName || `Nuevo Contenido ${this.cardCounter}`,
+      imageUrl: this.newModuleImageUrl || 'default-image-url.jpg'
     });
     this.cardCounter++;
+    this.resetForm();
+  }
+
+  cancel() {
+    this.resetForm();
+  }
+
+  resetForm() {
+    this.newModuleName = '';
+    this.newModuleImageUrl = '';
+    this.showForm = false;
   }
 
   editCard(id: number) {
@@ -25,7 +42,7 @@ export class AgregarNuevoContenidoComponent {
 
   deleteCard(id: number) {
     this.cards = this.cards.filter(card => card.id !== id);
-     if (this.cards.length === 0) {
+    if (this.cards.length === 0) {
       this.cardCounter = 1;
     }
   }
