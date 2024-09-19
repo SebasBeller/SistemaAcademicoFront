aac2a5c47434b8c39e6f52d2aee914912a88f684
SebasBeller <121450828+SebasBeller@users.noreply.github.com>
2024-09-19 09:07:10 -0400

    Ver material estudiante (#9)
    * Se agregan los servicios de la API con la IA y la de los pdfs
    
    * Se agregan los servicios de materiales
    
    ---------
    
    Co-authored-by: 987sebasBeller <sebastianbeller76@gmail.com>

---
+++ src/app/paginas/ver-materiales/ver-materiales.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ver-materiales',
+  standalone: true,
+  imports: [],
+  templateUrl: './ver-materiales.component.html',
+  styleUrl: './ver-materiales.component.sass'
+})
+export class VerMaterialesComponent {
+
+}
