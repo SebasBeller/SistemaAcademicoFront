5bae9f24af2f997599de12bbc8d1d2dd2137e96e
987sebasBeller <sebastianbeller76@gmail.com>
2024-09-22 09:37:33 -0400

    se crea servicio de materias

---
+++ src/app/interfaces/materia.ts
@@ -0,0 +1,6 @@
+export interface Materia {
+    id: number;
+    nombre: string;
+    area?: string | 'area Y';
+    paralelo: string;
+}
---
+++ src/app/paginas/ver-materias-docente/ver-materias-docente.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ver-materias-docente',
+  standalone: true,
+  imports: [],
+  templateUrl: './ver-materias-docente.component.html',
+  styleUrl: './ver-materias-docente.component.sass'
+})
+export class VerMateriasDocenteComponent {
+
+}
