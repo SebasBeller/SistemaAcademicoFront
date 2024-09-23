e4716a627a66dffaa18c10f6e5bb618843b5dcff
Wama85 <wi_an9@hotmail.com>
2024-09-23 06:54:15 -0400

    vista detalle

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,23 +5,14 @@ import { BuscarMateriaComponent } from './paginas/buscar-materia/buscar-materia.
 import { MostrarMateriaComponent } from './paginas/mostrar-materia/mostrar-materia.component';
 import { DetalleMateriaComponent } from './paginas/detalle-materia/detalle-materia.component';
 import {VerMaterialesComponent} from './paginas/ver-materiales/ver-materiales.component'
+
 export const routes: Routes = [
-    {path:'ver-material/:id',component:MaterialComponent
-
-    }, 
-    {
-      path: 'agregar-nuevo-contenido',
-      component: AgregarNuevoContenidoComponent, 
-      title: 'Agregar Nuevo Contenido'
-    },
-      {path:'mostrar-materia', component:MostrarMateriaComponent
-
-      },
-      {path:'detalle-materia', component:DetalleMateriaComponent},
-      {path : 'Buscar-Materia', component: BuscarMateriaComponent},
-
-      // parte Marlon >> Home
-      { path: 'agregar-material-docente', loadChildren: () => import('./paginas/agregar-material-docente/home/feature/home-routing') },
-      {path : 'ver-materiales', component: VerMaterialesComponent }
-      ];
+  { path: 'ver-material/:id', component: MaterialComponent },
+  { path: 'agregar-nuevo-contenido', component: AgregarNuevoContenidoComponent, title: 'Agregar Nuevo Contenido' },
+  { path: 'mostrar-materia', component: MostrarMateriaComponent },
+  { path: 'detalle-materia/:titulo', component: DetalleMateriaComponent }, // Modificación aquí
+  { path: 'Buscar-Materia', component: BuscarMateriaComponent },
+  { path: 'agregar-material-docente', loadChildren: () => import('./paginas/agregar-material-docente/home/feature/home-routing') },
+  { path: 'ver-materiales', component: VerMaterialesComponent }
+];
 
--- src/app/paginas/mostrar-materia/mostrar-materia.component.ts
+++ src/app/paginas/mostrar-materia/mostrar-materia.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-mostrar-materia',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,RouterLink],
   templateUrl: './mostrar-materia.component.html',
-  styleUrl: './mostrar-materia.component.sass'
+  styleUrls: ['./mostrar-materia.component.sass'] // Corregido a styleUrls
 })
 export class MostrarMateriaComponent {
+
+  // Definir un arreglo de materias
+  materias = [
+    {
+      titulo: 'Matemáticas',
+      imagen: 'assets/img/Recurso (1).png',
+      profesor: 'Juan Perez',
+      paralelo: '1er A'
+    },
+    {
+      titulo: 'Física',
+      imagen: 'assets/img/Recurso (4).png',
+      profesor: 'Ana Gomez',
+      paralelo: '1er B'
+    },
+    {
+      titulo: 'Química',
+      imagen: 'assets/img/Recurso (3).png',
+      profesor: 'Carlos Ruiz',
+      paralelo: '2do C'
+    }
+  ];
 
 }
