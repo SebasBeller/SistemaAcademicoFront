0ae3607ae6ea4a4d0551a595af6c7e52bf4f40f2
987sebasBeller <sebastianbeller76@gmail.com>
2024-09-25 08:25:53 -0400

    Se solucionan conflictos de merge

--- src/app/paginas/material/material.component.ts
+++ src/app/paginas/material/material.component.ts
@@ -1,47 +1,72 @@
 import { FormsModule } from '@angular/forms';
 import { PdfViewerModule } from 'ng2-pdf-viewer';
-import { PDFDocumentProxy, PDFPageProxy } from 'pdfjs-dist';
+import { PDFDocumentProxy } from 'pdfjs-dist';
 import {
   ChangeDetectionStrategy,
   Component,
   signal,
-  inject
+  inject,OnInit
 } from '@angular/core';
 import { MatExpansionModule } from '@angular/material/expansion';
 import {PdfService} from '../../servicios/pdf.service'
 import { MaterialService } from '../../servicios/material.service';
 import {ActivatedRoute} from '@angular/router'
-
+import { Material } from '../../interfaces/material';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-material',
   standalone: true,
-  imports: [FormsModule, PdfViewerModule, MatExpansionModule],
+  imports: [FormsModule, PdfViewerModule, MatExpansionModule, CommonModule],
   templateUrl: './material.component.html',
   styleUrl: './material.component.sass',
-  changeDetection: ChangeDetectionStrategy.OnPush,
+  // changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class MaterialComponent {
-  readonly panelOpenState = signal(false);
-
+  isOpen?:boolean; 
+  toggle() {
+    console.log("hola")
+    this.isOpen = !this.isOpen; 
+  }
+  readonly panelOpenState2 = signal(false);
+  panelOpenState: boolean = false;
   private src!:string;
   materialesServicio:MaterialService=inject(MaterialService) 
   route:ActivatedRoute=inject(ActivatedRoute) 
 
   private pdfService=inject(PdfService)
-  responseTeory?: string;
+  response?: string;
+
+  material?:Material;
+  
+  id_material?:number;
   constructor(){
-    let link=this.route.snapshot.params['id'];
-    this.src='/api'+link;
-    console.log(link)
+
+    this.id_material=this.route.snapshot.params['id'];
+
+  }
+
+  ngOnInit(): void {
+    this.isOpen = false; 
+    this.materialesServicio.encontrarMaterial(this.id_material).subscribe(
+      (data)=>{
+          this.material=data
+          this.src='/api'+this.material?.url
+          console.log(data)
+
+      },
+      error => {
+        console.error('Error en la petición GET:', error);
+      }
+    )
   }
   getSrc():string{
     return this.src;
   }
 
   async readPdf(pdf: PDFDocumentProxy) {
-    this.pdfService.readPdf(pdf).then(
+    this.pdfService.readPdf(pdf,this.material?.tipo).then(
       (data)=>{
-        this.responseTeory=data
+        this.response=data
       }
     ).catch(
       (e)=>console.log(e)
