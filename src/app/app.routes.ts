8b7ee83d5496ab50c069bd527918a86e42f868f4
987sebasBeller <sebastianbeller76@gmail.com>
2024-10-03 14:05:20 -0400

    Se muestra la asistencia de los estudiantes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,13 @@
-import { RouterModule,Routes } from '@angular/router';
+import { Routes } from '@angular/router';
 import { MaterialComponent } from './paginas/material/material.component'
 import { AgregarNuevoContenidoComponent } from './paginas/agregar-nuevo-contenido/agregar-nuevo-contenido.component';
 import { BuscarMateriaComponent } from './paginas/buscar-materia/buscar-materia.component';
 import { MostrarMateriaComponent } from './paginas/mostrar-materia/mostrar-materia.component';
 import { DetalleMateriaComponent } from './paginas/detalle-materia/detalle-materia.component';
-import { VerMaterialesComponent} from './paginas/ver-materiales/ver-materiales.component'
 import {VerMateriasDocenteComponent} from './paginas/ver-materias-docente/ver-materias-docente.component'
 import {VerMaterialesEstudianteComponent} from './paginas/ver-materia-estudiante/home/feature/home.component'
 import { LayoutComponent } from './layout/layout.component';
+import {RegistroAsistenciaDocentesComponent} from "./paginas/registro-asistencia-docentes/registro-asistencia-docentes.component"
 
 export const routes: Routes = [
   {
@@ -21,7 +21,9 @@ export const routes: Routes = [
       { path: 'Buscar-Materia', component: BuscarMateriaComponent },
       { path: 'agregar-material-docente/:id', loadChildren: () => import('./paginas/agregar-material-docente/home/feature/home-routing') },
       { path: 'ver-materias-docente', component: VerMateriasDocenteComponent },
-      { path: 'ver-materiales-estudiante/:id', component:VerMaterialesEstudianteComponent}
+      { path: 'ver-materiales-estudiante/:id', component:VerMaterialesEstudianteComponent},
+      { path: 'ver-asistencias-estudiantes', component:RegistroAsistenciaDocentesComponent}
+
     ]
 }
 ];
