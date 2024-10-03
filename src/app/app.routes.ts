5e2e52254c7a6582e40cf8f2e8e56213390a4752
SebasBeller <121450828+SebasBeller@users.noreply.github.com>
2024-10-03 18:24:42 -0400

    Rama joel navarro (#23)
    * Mostrar detalle Materia
    
    * Agregando estilos
    
    * Agregando estilos
    
    * Se añadio Lista de estudiantes para el profesor
    
    * Mejoras de estilos
    
    * Ultimas mejoras de estilos
    
    ---------
    
    Co-authored-by: joel.navarro@ucb.edu.bo <joel.navarro@ucb.edu.bo>
    Co-authored-by: 987sebasBeller <sebastianbeller76@gmail.com>

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,15 @@
-import { Routes } from '@angular/router';
+import { RouterModule,Routes } from '@angular/router';
 import { MaterialComponent } from './paginas/material/material.component'
 import { AgregarNuevoContenidoComponent } from './paginas/agregar-nuevo-contenido/agregar-nuevo-contenido.component';
 import { BuscarMateriaComponent } from './paginas/buscar-materia/buscar-materia.component';
 import { MostrarMateriaComponent } from './paginas/mostrar-materia/mostrar-materia.component';
 import { DetalleMateriaComponent } from './paginas/detalle-materia/detalle-materia.component';
+import {VerMateriasDocenteComponent} from './paginas/ver-materias-docente/ver-materias-docente.component';
+import { VerMaterialesComponent} from './paginas/ver-materiales/ver-materiales.component'
 import {VerMateriasDocenteComponent} from './paginas/ver-materias-docente/ver-materias-docente.component'
 import {VerMaterialesEstudianteComponent} from './paginas/ver-materia-estudiante/home/feature/home.component'
 import { LayoutComponent } from './layout/layout.component';
+import { ListaEstudiantesProfesorComponent } from './paginas/lista-estudiantes-profesor/lista-estudiantes-profesor.component';
 import {RegistroAsistenciaDocentesComponent} from "./paginas/registro-asistencia-docentes/registro-asistencia-docentes.component"
 
 export const routes: Routes = [
@@ -17,13 +20,13 @@ export const routes: Routes = [
       { path: 'ver-material/:id', component: MaterialComponent },
       { path: 'agregar-nuevo-contenido/:id_dicta', component: AgregarNuevoContenidoComponent, title: 'Agregar Nuevo Contenido' },
       { path: 'mostrar-materia', component: MostrarMateriaComponent },
-      { path: 'detalle-materia/:id_dicta', component: DetalleMateriaComponent }, // Modificación aquí
+      { path: 'detalle-materia/:id_dicta', component: DetalleMateriaComponent },
       { path: 'Buscar-Materia', component: BuscarMateriaComponent },
       { path: 'agregar-material-docente/:id', loadChildren: () => import('./paginas/agregar-material-docente/home/feature/home-routing') },
       { path: 'ver-materias-docente', component: VerMateriasDocenteComponent },
       { path: 'ver-materiales-estudiante/:id', component:VerMaterialesEstudianteComponent},
-      { path: 'ver-asistencias-estudiantes', component:RegistroAsistenciaDocentesComponent}
-
-    ]
-}
+      { path: 'lista-estudiantes-profesor', component:ListaEstudiantesProfesorComponent},
+        { path: 'ver-asistencias-estudiantes', component:RegistroAsistenciaDocentesComponent}
+    ]
+  }
 ];
