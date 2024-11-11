b7e016fcd28ab08eff8291a2213e56ddf7a6167e
987sebasBeller <sebastianbeller76@gmail.com>
2024-11-11 14:39:26 -0400

    Se implementa el crear materias desde administrador

--- src/app/paginas/formulario-agregar-materia/formulario-agregar-materia.component.ts
+++ src/app/paginas/formulario-agregar-materia/formulario-agregar-materia.component.ts
@@ -3,11 +3,24 @@ import { MAT_DIALOG_DATA, MatDialogRef, MatDialogModule } from '@angular/materia
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button'; 
-import {Paralelo} from '../../interfaces/paralelo'
+import {Paralelo} from '../../interfaces/paralelo';
+import {MensajeService} from '../mensaje/mensaje.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
 @Component({
   selector: 'app-formulario-agregar-materia',
   standalone: true,
-  imports: [CommonModule, FormsModule, MatDialogModule, MatButtonModule],
+  imports: [CommonModule,
+     FormsModule,
+     MatDialogModule,
+     MatButtonModule,
+     MatFormFieldModule,
+     MatInputModule,
+     MatDatepickerModule,
+     MatNativeDateModule,
+    ],
   templateUrl: './formulario-agregar-materia.component.html',
   styleUrl: './formulario-agregar-materia.component.sass',
 
@@ -19,13 +32,17 @@ export class FormularioAgregarMateriaComponent {
   
   constructor(
     public dialogRef: MatDialogRef<FormularioAgregarMateriaComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: any
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private readonly mensajeService:MensajeService
   ) {
     this.newMateriaName = data.name || '';
     this.paralelos = data.paralelos || '';
   }
   onAdd() {
-    console.log(this.paralelo)
+    if(!this.newMateriaName || ! this.paralelo){
+      this.mensajeService.mostrarMensajeError("Error!!!","Complete todos los campos!!")
+      return;
+    }
     this.dialogRef.close({
       name: this.newMateriaName,
       paralelo: this.paralelo,
--- src/app/paginas/formulario-asignar-materia-docente/formulario-asignar-materia-docente.component.ts
+++ src/app/paginas/formulario-asignar-materia-docente/formulario-asignar-materia-docente.component.ts
@@ -3,11 +3,25 @@ import { MAT_DIALOG_DATA, MatDialogRef, MatDialogModule } from '@angular/materia
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button'; 
-import {Profesor} from '../../interfaces/profesor'
+import {Profesor} from '../../interfaces/profesor';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import {MensajeService} from '../mensaje/mensaje.component'
 @Component({
   selector: 'app-formulario-asignar-materia-docente',
   standalone: true,
-  imports: [CommonModule, FormsModule, MatDialogModule, MatButtonModule],
+  imports: [CommonModule, 
+    FormsModule, 
+    MatDialogModule, 
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+
+  ],
   templateUrl: './formulario-asignar-materia-docente.component.html',
   styleUrl: './formulario-asignar-materia-docente.component.sass'
 })
@@ -18,17 +32,21 @@ export class FormularioAsignarMateriaDocenteComponent {
   
   constructor(
     public dialogRef: MatDialogRef<FormularioAsignarMateriaDocenteComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: any
+    @Inject(MAT_DIALOG_DATA) public data: any,private readonly mensajeService:MensajeService
   ) {
     this.profesores= data.profesores || '';
     this.fecha = data.fecha || new Date();
   }
   onAdd() {
-    console.log(this.profesor)
+    if(!this.profesor){
+      this.mensajeService.mostrarMensajeError("Error!!","Debe asignar un profesor para la materia");
+      return;
+    }
     this.dialogRef.close({
       profesor: this.profesor,
       fecha: this.fecha,
     });
+    this.mensajeService.mostrarMensajeExito("Exito!!","Se asigno al profesor con exito!!")
   }
 
   onCancel() {
